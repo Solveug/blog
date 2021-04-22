c8fada0072207cb75c3406ecdc7e7457f26da92f
Aleksandra <solveug@rambler.ru>
2021-04-22 13:25:38 +0300

    create table Posts

--- app.rb
+++ app.rb
@@ -13,6 +13,16 @@ before do
   init_db
 end
 
+configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
+  (
+  id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  created_date  DATE,
+  content TEXT
+  )'
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
@@ -22,8 +32,6 @@ get '/new_post' do
 end
 
 post '/new_post' do
-
-
   content = params[:content]
 
   erb "You typed #{content}"
